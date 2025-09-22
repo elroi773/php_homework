687f7089fd6e80b28ac331c589ed919d2e191773
elroi773 <s2457@e-mirim.hs.kr>
2025-09-22 15:02:13 +0900

    예제 파일 입니다

---
+++ input.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+</head>
+
+<body>
+	<form action="login.php" method="get">
+		아이디 : <input type="text" name="id"><br>
+		비밀번호 : <input type="password" name="pw"><br>
+		<input type="submit" value="로그인">
+	</form>
+</body>
+
+</html>
