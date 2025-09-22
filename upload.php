ab6d123a13b1a02aef28586a10884097c6241c23
elroi773 <s2457@e-mirim.hs.kr>
2025-09-23 08:39:17 +0900

    예제 파일 입니다

---
+++ upload.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+</head>
+
+<body>
+	<form action="getfiles.php" method="post" enctype="multipart/form-data">
+		업로드할 파일을 선택하세요.<br>
+		<input type="file" name="upload"><br><br>
+		<input type="submit" value="업로드">
+	</form>
+</body>
+
+</html>
